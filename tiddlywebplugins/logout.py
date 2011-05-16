cc520ec6a47a65334c775bec6dd039cab350f61e
Chris Dent <chris.dent@gmail.com>
2011-05-16 12:14:49 +0100

    pep8 and pylint cleanups to logout
    To keep things fresh.

--- tiddlywebplugins/logout.py
+++ tiddlywebplugins/logout.py
@@ -1,6 +1,10 @@
 """
-A simple logout plugin that works with the simple_cookie
-extractor.
+A simple logout plugin.
+
+This works with the simple_cookie extractor: a POST to /logout
+will expire the tiddlyweb_user cookie.
+
+See also http://tiddlyweb.com and http://github.com/tiddlyweb
 """
 
 from tiddlyweb.web.util import server_base_url
@@ -11,8 +15,7 @@ import time
 
 def logout(environ, start_response):
     """
-    Break the web by allowing a logout on a GET request.
-    And then break it further by sending a bad redirect.
+    Expire the tiddlyweb_user cookie when a POST is received.
     """
     uri = environ.get('HTTP_REFERER',
             server_base_url(environ) +
@@ -21,22 +24,28 @@ def logout(environ, start_response):
     cookie = Cookie.SimpleCookie()
     cookie['tiddlyweb_user'] = ''
     cookie['tiddlyweb_user']['path'] = '%s/' % path
+
     if 'MSIE' in environ.get('HTTP_USER_AGENT', ''):
         cookie['tiddlyweb_user']['expires'] = time.strftime(
-                '%a, %d-%m-%y %H:%M:%S GMT', time.gmtime(time.time()-600000))
+                '%a, %d-%m-%y %H:%M:%S GMT', time.gmtime(time.time() - 600000))
     else:
         cookie['tiddlyweb_user']['max-age'] = '0'
+
     cookie_output = cookie.output(header='')
     start_response('303 See Other', [
         ('Set-Cookie', cookie_output),
         ('Expires', time.strftime(
-            '%a, %d %b %Y %H:%M:%S GMT', time.gmtime(time.time()-600000))),
+            '%a, %d %b %Y %H:%M:%S GMT', time.gmtime(time.time() - 600000))),
         ('Cache-Control', 'no-store'),
         ('Location', uri),
         ])
+
     return [uri]
 
 
 def init(config):
+    """
+    Add the logout handler on /logout.
+    """
     if 'selector' in config:
         config['selector'].add('/logout', POST=logout)
